1f8cd8e2e7a6fa4edf2845dd448d17a67abaa63f
Mark Devenyi <markdevenyidev@gmail.com>
2025-02-10 17:11:38 -0500

    refactor(cli): remove unnecessary CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
 use clap::Parser;
 
-use serde::Serialize;
-
-#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
-enum Arch {
-    #[default]
-    X86_64,
-}
-
-#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
-enum OS {
-    #[default]
-    Linux,
-    Windows,
-}
-
-#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
-enum FileFormat {
-    #[default]
-    Object,
-    PE,
-    Elf,
-}
-
-impl core::fmt::Display for Arch {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
+mod loader;
 
 /// An x64 to ARM64 binary recompiler
 #[derive(Parser, Debug)]
@@ -36,18 +9,6 @@ struct Args {
     /// Path to binary file to recompile
     #[arg(short, long)]
     bin: String,
-
-    /// Binary format
-    #[arg(short, long)]
-    format: FileFormat,
-
-    /// Architecture of binary
-    #[arg(short, long)]
-    arch: Arch,
-
-    /// Operating system
-    #[arg(short, long)]
-    os: OS,
 }
 
 fn main() {
